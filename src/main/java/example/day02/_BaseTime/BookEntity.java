c2e056849afca1c9b97696d24947fbb923e4d6a2
itdanjalog <kgs2072@naver.com>
2025-04-02 11:07:08 +0900

    day97

--- src/main/java/example/day02/_BaseTime/BookEntity.java
+++ src/main/java/example/day02/_BaseTime/BookEntity.java
@@ -11,19 +11,14 @@ public class BookEntity extends BaseTime {
     @Id // PRIMARY KEY
     @GeneratedValue( strategy = GenerationType.IDENTITY) // auto_increment
     private int 도서번호;
-
     @Column( nullable = false , length = 100 ) // not null , varchar(30)
     private String 도서명;
-
     @Column( nullable = false , length = 30 ) // not null  , varchar(100)
     private String 저자;
-
     @Column( nullable = false , length = 50 ) // not null  , varchar(50)
     private String 출판사;
-
     @Column
     private int 출판연도;
-
 }
 
 
