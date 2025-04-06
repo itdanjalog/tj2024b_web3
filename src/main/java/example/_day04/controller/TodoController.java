5c44b51b1762591a595605ec7800d691cd44be68
MSI <kgs2072@naver.com>
2025-04-07 02:47:37 +0900

    -

--- src/main/java/example/_day04/controller/TodoController.java
+++ src/main/java/example/_day04/controller/TodoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/day04/todos") // 공통 URL prefix
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class TodoController {
 
     private final TodoService todoService;
