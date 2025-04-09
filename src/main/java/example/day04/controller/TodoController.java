94c2151d05a0c1aa04b1df04b3d28456a2ea0afa
itdanjalog <kgs2072@naver.com>
2025-04-09 13:24:42 +0900

    day100

--- src/main/java/example/day04/controller/TodoController.java
+++ src/main/java/example/day04/controller/TodoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/day04/todos")
 @RequiredArgsConstructor
+@CrossOrigin("*") // 플러터 dio (web사용시) 테스트 용도
 public class TodoController {
 
     private final TodoService todoService;
