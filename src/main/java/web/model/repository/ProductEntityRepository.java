7ad1415c5a5b2a26b4b36629e12c5528948d5161
itdanjalog <kgs2072@naver.com>
2025-04-24 12:27:18 +0900

    day112

--- src/main/java/web/model/repository/ProductEntityRepository.java
+++ src/main/java/web/model/repository/ProductEntityRepository.java
@@ -1,5 +1,7 @@
 package web.model.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -26,6 +28,14 @@ public interface ProductEntityRepository
         // select * from product where cno = :cno
     @Query( value = "select * from product where cno = :cno" , nativeQuery = true)
     List<ProductEntity> nativeQuery1( long cno );
+
+        // (*) 카테고리별(cno) 와 제품키워드(keyword) 검색 + 페이징처리
+    // @Query( value = "SQL 작성" , nativeQuery = true )
+    // SQL 문에서 매개변수 사용시 매개변수앞에 :(콜론)
+    @Query( value = "SELECT * FROM product " +
+            " WHERE ( :cno IS NULL OR :cno = 0 OR cno :cno ) " + // java == null vs sql : IS NULL
+            " AND ( :keyword IS NULL OR pname LIKE %:keyword% )" , nativeQuery = true)
+    Page<ProductEntity> findBySearch( Long cno , String keyword , Pageable pageable );
 
     // 방법4* JPQL , 규칙 : 자바만든 sql 코드/메소드
 
